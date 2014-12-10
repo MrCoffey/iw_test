cb3897570fb372f343f18fbd86b33bcda59a6d11
Jorge Madrid <jorjilio@hotmail.com>
2014-12-10 16:05:26 -0500

    todo_items funcionando

---
+++ app/controllers/todo_items_controller.rb
@@ -0,0 +1,34 @@
+class TodoItemsController < ApplicationController
+	before_action :set_todo_lists
+	before_action :set_todo_items, except: [:create]
+
+	def create
+		@todo_item = @todo_list.todo_items.create(todo_items_params)
+		redirect_to @todo_list, notice: "New item created!"
+	end
+
+	def destroy
+		if @todo_item.destroy
+			#Forma corta
+			redirect_to @todo_list, notice: "Item deleted"
+		else
+			#Forma larga
+			redirect_to @todo_list
+			flash[:error] = "Whoops we have a truoble"
+		end		
+	end
+
+	private
+
+	def todo_items_params
+		params[:todo_item].permit(:title)
+	end
+
+	def set_todo_lists
+		@todo_list = TodoList.find(params[:todo_list_id])
+	end
+
+	def set_todo_items
+		@todo_item = @todo_list.todo_items.find(params[:id])
+	end
+end
